dd8aea2126faf16ae8c8a4eca202df81f84dd7c0
Joel Ligma <66591896+JoelLigma@users.noreply.github.com>
2022-01-30 14:57:09 +0000

    Upload file

---
+++ Array/167.Two_Sum_II.py
@@ -0,0 +1,32 @@
+"""
+167. Two Sum II - Input Array Is Sorted
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+"""
+
+# Solution 1:
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = dict()
+        for i, v in enumerate(numbers):
+            if target - v in d:
+                return [d[target - v]+1, i+1]
+            d[v] = i
+            
+# Space/time complexity analysis: Space: O(n) & Time: O(n) 
+# adding 1 to the indices is O(1) but dictionary may scale with input array size
+
+# Solution 2:
