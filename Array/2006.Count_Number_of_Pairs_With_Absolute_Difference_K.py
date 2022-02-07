e4dd2e9709fe4c577ec8096642c8e6d18a6cf42d
Joel Ligma <66591896+JoelLigma@users.noreply.github.com>
2022-02-07 19:13:56 +0000

    Add alternative solution

--- Array/2006.Count_Number_of_Pairs_With_Absolute_Difference_K.py
+++ Array/2006.Count_Number_of_Pairs_With_Absolute_Difference_K.py
@@ -14,7 +14,7 @@ Output: 4
 """
 
 
-# Solution 1:
+# Solution 1: Brute Force
 class Solution(object):
     def countKDifference(self, nums, k):
         """
@@ -22,12 +22,37 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = 0
+        counter = 0
         for i in range(len(nums)):
             for j in range(i,len(nums)):
                 if abs(nums[i] - nums[j]) == k:
-                    count += 1   
-        return count
+                    counter += 1   
+        return counter
       
-# Time complexity: O(n**2)
+# Time complexity: O(n^2)
 # Space complexity: O(1)
+
+
+# Solution 2: Hashmap
+class Solution(object):
+    def countKDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """ 
+        import collections
+        """
+        Defaultdict() is part of the collections module. Defaultdict means that if a key is not found in the dictionary,
+        then instead of a KeyError being thrown a new entry is created. The type of this new entry is given by the
+        argument of defaultdict, e.g. defaultdict(int).
+        """
+        d = defaultdict(int)
+        c = 0
+        for num in nums:
+            c += d[num-k] + d[num+k]
+            d[num] += 1
+        return c
+   
+# Time complexity: O(n)
+# Space complexity: O(n)
