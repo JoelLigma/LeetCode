6e097f451aab2c530d0264c4c4c5258fbc0a272d
Joel Ligma <66591896+JoelLigma@users.noreply.github.com>
2022-05-05 12:10:37 +0100

    Upload file

---
+++ Array/1346.Check_If_N_and_Its_Double_Exist.py
@@ -0,0 +1,46 @@
+"""
+1346. Check If N and Its Double Exist
+
+Given an array arr of integers, check if there exists two integers N and M such that N is the double of M ( i.e. N = 2 * M).
+
+More formally check if there exists two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+ 
+
+Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: N = 10 is the double of M = 5,that is, 10 = 2 * 5.
+"""
+
+class Solution(object):
+    # Solution 1: nested loop
+    def checkIfExist(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if i != j and arr[i] == arr[j] * 2:
+                    return True
+        return False
+    # time/space complexity: O(n**2)/O(1)
+      
+      
+    # Solution 2: Using enumerate (slightly faster)
+    def checkIfExist(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        for i,x in enumerate(arr):
+            for j,y in enumerate(arr):
+                if i != j and x == y * 2:
+                    return True
+        return False
+    # time/space complexity: O(n**2)/O(1)     
