4bfafa7b4f524f09d2b864b204d294411904b334
Joel Ligma <66591896+JoelLigma@users.noreply.github.com>
2023-08-18 01:45:12 +0100

    Update 125.Valid_Palindrome.py

--- String/125.Valid_Palindrome.py
+++ String/125.Valid_Palindrome.py
@@ -21,3 +21,19 @@ class Solution:
         return False
 # time complexity: O(s), where s is the size of the string
 # space complexity: O(n), where n is the size of the processed string. We may also be able to reassign s to the processed string to save memory as it could make it O(1).
+
+    def isPalindrome2(self, s: str) -> bool:
+        res = ""
+        for ch in s:
+            ch = ch.lower()
+            if ch.isalnum():
+                res += ch
+        
+        # below can be replaced by res == res[::-1]
+        reversed_res = ""
+        for i in range(len(res) -1, -1, -1):
+            reversed_res += res[i]
+        return res == reversed_res
+
+# Time complexity: O(n), where n is the size of the input str
+# Space complexity: O(n), where n is the number of alpha numeric characters in the input str
