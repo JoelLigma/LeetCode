fc0942df9b7acc1ce4aed6323cf2f789b93cafaa
Joel Ligma <66591896+JoelLigma@users.noreply.github.com>
2023-09-02 19:37:38 +0100

    Update 20.Valid_Parenthesis.py

--- String/20.Valid_Parenthesis.py
+++ String/20.Valid_Parenthesis.py
@@ -2,16 +2,15 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         bracket_map = {"(": ")", "[": "]",  "{": "}"}
-        open_brackets = set(["(", "[", "{"])
 
         for ch in s:
-            if ch in open_brackets:
+            if ch in bracket_map:
                 stack.append(ch)
             elif stack and ch == bracket_map[stack[-1]]:
                 stack.pop()
             else:
                 return False
-        return not stack
+        return len(stack) == 0
 
 # Time complexity: O(n), where n is the size of s
 # Space complexity: O(n), where n is the size of the stack in the worst case of opening brackets only
